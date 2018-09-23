04e2fe89e6bed90017965a55715bb4ef1cb40d06
Leo Rodriguez <leo45rodriguez@hotmail.com>
2018-09-23 13:53:48 -0400

    Changed package to sample

--- Robocode/src/sample/DaRealCliff.java
+++ Robocode/src/sample/DaRealCliff.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://robocode.sourceforge.net/license/epl-v10.html
  */
-package SLLR;
+package sample;
 
 
 import robocode.HitByBulletEvent;
